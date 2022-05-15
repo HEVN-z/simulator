c6e68a699cdc28d8fe80e53245f231056e41899f
HEVNz <e-exe-e@live.com>
2022-05-16 04:49:04 +0700

    g

---
+++ _db.py
@@ -0,0 +1,18 @@
+import pymongo
+import os
+import time
+from dotenv import load_dotenv
+load_dotenv()
+
+client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = client.users
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": time.strftime("%c")}
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+posts.delete_many({})
+
+
+print(db)
