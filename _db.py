6e19a84bd3fc6cf3baa9fac4e5ae26d528a175c9
HEVNz <e-exe-e@live.com>
2022-05-18 05:21:11 +0700

    g

--- _db.py
+++ _db.py
@@ -1,28 +1,42 @@
+from tracemalloc import start
 import pymongo
 import os
 import time
 from dotenv import load_dotenv
+
+
+
 load_dotenv()
 
-client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+client = pymongo.MongoClient(os.getenv("LOCAL_URI"))
+
 db = client.users
-post = {"author": "Mike",
+post = {"_id":0,
+        "author": "Mike",
         "text": "My first blog post!",
         "tags": ["mongodb", "python", "pymongo"],
-        "date": time.strftime("%c")}
+        "date": time.strftime("%c"),
+        "email": "555@gmail.com"}
 posts = db.posts
+start = time.time()
 post_id = posts.insert_one(post).inserted_id
-posts.delete_many({})
 
-def get_posts():
-    return list(posts.find())
+def get_from_email(email):
+    return posts.find_one({'email': email})
 
 def add_post(post):
     post_id = posts.insert_one(post).inserted_id
     return post_id
 
-def set_post(post):
-    posts.update_one({"_id": post["_id"]}, {"$set": post})
+def set_author(email, author):
+    posts.update_one({"email": email}, {"$set": {"author": author}})
 
+# print(db)
+print(get_from_email("555@gmail.com")['author'])
+print(post["_id"])
+set_author("555@gmail.com", "Alice")
+print(get_from_email("555@gmail.com")['author'])
+posts.delete_many({})
+end = time.time()
 
-print(db)
+print(end - start)
